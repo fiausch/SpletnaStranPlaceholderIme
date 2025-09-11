89b235252f811d1a721c2856c6660c756768f56e
Matic Fijavz <matic.fijavz.19625@dijak.sc-celje.si>
2025-09-11 18:59:02 +0200

    popravil shell scripto

--- scripte/ubuntulaunch.sh
+++ scripte/ubuntulaunch.sh
@@ -1,2 +1,3 @@
-qemu-img create -f qcow2 Image.img 40G 
+#!/bin/bash
+qemu-img create -f qcow2 Image.img 50G 
 qemu-system-x86_64 -enable-kvm -cdrom ubuntu-24.04.3-live-server-amd64.iso -boot menu=on -drive file=Image.img -m 4G -smp 9 -vga virtio -display sdl,gl=on
